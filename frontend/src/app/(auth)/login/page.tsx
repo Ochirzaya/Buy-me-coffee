c02fc7d8dac191767870afca7ba42ff9f977b29e
24LP3863 24LP3863 <24LP3863@G302-CC0089.local>
2025-05-15 09:50:24 +0800

    login sign up button

--- frontend/src/app/(auth)/login/page.tsx
+++ frontend/src/app/(auth)/login/page.tsx
@@ -1,8 +1,14 @@
+import { Button } from "@/components/ui/button";
 import { LoginSection } from "../component/LoginSection";
 
 export default function LoginPage() {
   return (
     <div className="w-1/2 h-full flex items-center justify-center">
+      <div className="absolute flex top-8 right-20">
+        <Button className="w-[73px] h-10 rounded-md bg-[#F4F4F5] px-4 py-2">
+          <p className="text-black">Sign up</p>
+        </Button>
+      </div>
       <LoginSection />
     </div>
   );
